cc971f7cfa50cc9b8227bf0c4020497af719dd06
Mateusz Szymczak <mszymczak710@o2.pl>
2023-09-16 05:09:34 +0200

    sort imports

--- brainiacs/src/app/app-routing.module.ts
+++ brainiacs/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
 
 const routes: Routes = [
 	{
--- brainiacs/src/app/app.module.ts
+++ brainiacs/src/app/app.module.ts
@@ -1,35 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { HomeComponent } from './components/home/home.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducers } from './components/users/state/reducers/users.reducer';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NgModule } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 import { ToastrModule } from 'ngx-toastr';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { reducers } from './components/users/state/reducers/users.reducer';
 
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http, './assets/i18n/shared/', '.json');
 }
 
 @NgModule({
-	declarations: [AppComponent, NavigationComponent, HomeComponent],
+	declarations: [AppComponent, HomeComponent, NavigationComponent],
 	imports: [
+		AppRoutingModule,
 		BrowserModule,
 		BrowserAnimationsModule,
-		AppRoutingModule,
-		HttpClientModule,
-		StoreModule.forRoot({ users: reducers }),
 		EffectsModule.forRoot([]),
+		HttpClientModule,
 		NgbDropdownModule,
 		StoreDevtoolsModule.instrument({}),
+		StoreModule.forRoot({ users: reducers }),
 		ToastrModule.forRoot({
 			timeOut: 1500,
 			preventDuplicates: true,
